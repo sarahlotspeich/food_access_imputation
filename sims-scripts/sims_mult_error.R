e4ea358e51211c1b3f7ff9cf794ee6eac7d335c8
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-02-12 17:13:37 -0500

    Rerun mult_error sims with Piedmont params

--- sims-scripts/sims_mult_error.R
+++ sims-scripts/sims_mult_error.R
@@ -1,47 +1,48 @@
-# Run once
-# if needed: install.packages("devtools") 
-# then: 
-# devtools::install_github("sarahlotspeich/possum", 
-#                          ref = "main") 
+# Install libraries
+## Run once
+## if needed: install.packages("devtools") 
+## then: 
+## devtools::install_github("sarahlotspeich/possum", 
+##                          ref = "main") 
 
-library(possum, ## for multiple imputation estimator
-        lib.loc = "/home/lotspes/R/x86_64-pc-linux-gnu-library/4.0") 
+# Load libraries
+library(possum) ## for multiple imputation estimator
+library(tictoc) ## to calculate runtime
 
 # Random seed to be used for each simulation setting
-args = commandArgs(TRUE)
-## When running on the cluster, give each array a unique seed by adding the array ID to 11422
-sim_seed = 11422 + as.integer(args)
+sim_seed = 11422
 
 # Number of replicates per simulation setting
-num_reps = 100
+num_reps = 1000
 ## Note: You may want to run this code was run in parallel a cluster instead of locally, as it can be slow.
 
 # Set parameters that won't be varied in the loop
 ## These values will be set as the defaults in the sim_data() function for convenience
-fix_avg_prev = 0.07 ## average prevalence
+fix_beta0 = -2.7 ## outcome model intercept (leads to ~ 7% prevalence)
 fix_beta1 = log(1.01) ## log prevalence ratio
-fix_pV = 0.1 ## proportion of neighborhoods to be queried
+fix_muW = 0.7 ## error mean
+fix_q = 0.1 ## proportion of neighborhoods to be queried
 
 # --------------------------------------------------------------------
 # Function to simulate data (arguments defined as follows)
 ## N = number of neighborhoods (sample size)
-## avg_prev = approximate outcome prevalence
+## beta0 = model intercept
 ## beta1 = log prevalence ratio 
-## maxW = max of the measurement error distribution
+## muU = mean of the measurement error distribution
+## sigmaU = standard deviation of the measurement error distribution
 # --------------------------------------------------------------------
-sim_data = function(N, avg_prev = fix_avg_prev, beta1 = fix_beta1, maxW) {
-  ## Get model intercept
-  beta0 = log(avg_prev)
-  
+sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, muW = fix_muW, sigmaW) {
   ## Simulate true (map-based) proximity to grocery store
   X = rgamma(n = N,
              shape = 1,
              scale = 2.5) 
   
   ## Simulate random errors
-  W = runif(n = N, 
-            min = 1, 
-            max = maxW)
+  W = truncnorm::rtruncnorm(n = N, 
+                            a = 0, 
+                            b = 1, 
+                            mean = muW, 
+                            sd = sigmaW)
   
   ## Construct error-prone (straight-line) proximity to grocery store
   Xstar = X * W ### assuming multiplicative measurement error model
@@ -62,27 +63,28 @@ sim_data = function(N, avg_prev = fix_avg_prev, beta1 = fix_beta1, maxW) {
   return(dat)
 }
 
-# Loop over different sample sizes: N = 100, 340, 2200
-for (N in c(100, 340, 2200)) {
-  # And max of multiplicative error: 1.25, 1.4, 1.6, 2
-  for (m in c(1.25, 1.4, 1.6, 2)) {
+# Loop over different sample sizes: N = 390 (Piedmont Triad), 2200 (all of NC)
+for (N in c(390, 2200)) {
+  tic(paste("Sims with N =", N)) ## Start counting runtime for sims with current sample size N
+  # And error standard deviation: 0.1, 0.2, 0.4, 0.8, 1
+  for (sigma in c(0.1, 0.15, 0.2)){
     # Be reproducible
     set.seed(sim_seed) ## set random seed
     
     # Create dataframe to save results for setting
     sett_res = data.frame(sim = paste(sim_seed, 1:num_reps, sep = "-"), 
-                          N, beta1 = fix_beta1, maxW = m, pV = fix_pV, avg_prev = NA, ## simulation setting
-                          beta_gs = NA, se_beta_gs = NA, ## gold standard analysis
-                          beta_n = NA, se_beta_n = NA, ## naive analysis
-                          beta_cc = NA, se_beta_cc = NA, ## complete case analysis
-                          beta_imp = NA, se_beta_imp = NA ## imputation analysis
+                          N, beta0 = fix_beta0, beta1 = fix_beta1, muU = fix_muW, sigmaU = sigma, q = fix_q, avg_prev = NA, ## simulation setting
+                          beta0_gs = NA, se_beta0_gs = NA, beta1_gs = NA, se_beta1_gs = NA, ## gold standard analysis
+                          beta0_n = NA, se_beta0_n = NA, beta1_n = NA, se_beta1_n = NA, ## naive analysis
+                          beta0_cc = NA, se_beta0_cc = NA, beta1_cc = NA, se_beta1_cc = NA, ## complete case analysis
+                          beta0_imp = NA, se_beta0_imp = NA, beta1_imp = NA, se_beta1_imp = NA ## imputation analysis
     )
-
+    
     # Loop over replicates 
     for (r in 1:num_reps) {
       # Generate data
       dat = sim_data(N  = N, ## sample size
-                     maxW = m) ## max of multiplicative error
+                     sigmaW = sigma) ## error standard deviation
       
       # Save average neighborhood prevalence
       sett_res$avg_prev[r] = mean(dat$Cases / dat$P)
@@ -92,20 +94,20 @@ for (N in c(100, 340, 2200)) {
                    family = poisson,
                    offset = log(P),
                    data = dat)
-      sett_res$beta_gs[r] = coefficients(fit_gs)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_gs[r] = sqrt(diag(vcov(fit_gs)))[2] ## and its standard error
+      sett_res[r, c("beta0_gs", "beta1_gs")] = coefficients(fit_gs) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_gs", "se_beta1_gs")] = sqrt(diag(vcov(fit_gs))) ## and its standard error
       
       # Fit the gold standard model
       fit_n = glm(formula = Cases ~ Xstar, 
                   family = poisson,
                   offset = log(P),
                   data = dat)
-      sett_res$beta_n[r] = coefficients(fit_n)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_n[r] = sqrt(diag(vcov(fit_n)))[2] ## and its standard error
+      sett_res[r, c("beta0_n", "beta1_n")] = coefficients(fit_n) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_n", "se_beta1_n")] = sqrt(diag(vcov(fit_n))) ## and its standard error
       
       # Select subset of neighborhoods/rows for map-based measures
       query_rows = sample(x = 1:N, 
-                          size = ceiling(fix_pV * N), 
+                          size = ceiling(fix_q * N), 
                           replace = FALSE)
       
       # Make X NA/missing for rows not in selected subset (query_rows)
@@ -116,21 +118,26 @@ for (N in c(100, 340, 2200)) {
                    family = poisson,
                    offset = log(P),
                    data = dat)
-      sett_res$beta_cc[r] = coefficients(fit_cc)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_cc[r] = sqrt(diag(vcov(fit_cc)))[2] ## and its standard error
-
+      sett_res[r, c("beta0_cc", "beta1_cc")] = coefficients(fit_cc) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_cc", "se_beta1_cc")] = sqrt(diag(vcov(fit_cc))) ## and its standard error
+      
       # Fit the pooled MI model
-      fit_imp = possum::impPossum(imputation_formula = X ~ Xstar + log(Cases), 
-                                  analysis_formula = Cases ~ X + offset(log(P)), 
-                                  data = dat, 
-                                  B = 20)
-      sett_res$beta_imp[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
-      sett_res$se_beta_imp[r] = fit_imp$Standard.Error[2] ## and its standard error
+      fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Cases), 
+                          analysis_formula = Cases ~ X + offset(log(P)), 
+                          data = dat, 
+                          B = 20)
+      sett_res[r, c("beta0_imp", "beta1_imp")] = fit_imp$Estimate ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_imp", "se_beta1_imp")] = fit_imp$Standard.Error ## and its standard error
       
       # Save results
       write.csv(x = sett_res,
-                file = paste0("mult_error/proximity_N", N, "_maxW", 10 * m, "_seed", sim_seed, ".csv"), 
+                file = paste0("mult_error/proximity_N", N, "_sigmaW", 100 * sigma, "_seed", sim_seed, ".csv"), 
                 row.names = F)
     }
   }
+  toc() ## End runtime for sims with current sample size N
 }
+
+# Timing from tictoc:
+## Sims with N = 390: 128.43 sec elapsed
+## Sims with N = 2200: 334.341 sec elapsed
