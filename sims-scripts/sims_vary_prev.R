7d57a0dce593ee04a7e67a3c498f8568a16d4e15
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-02-12 16:02:28 -0500

    Commit rerun sims vary_prev with Piedmont params

--- sims-scripts/sims_vary_prev.R
+++ sims-scripts/sims_vary_prev.R
@@ -1,38 +1,37 @@
-# Run once
-# if needed: install.packages("devtools") 
-# then: 
-# devtools::install_github("sarahlotspeich/possum", 
-#                          ref = "main") 
+# Install libraries
+## Run once
+## if needed: install.packages("devtools") 
+## then: 
+## devtools::install_github("sarahlotspeich/possum", 
+##                          ref = "main") 
 
+# Load libraries
 library(possum) ## for multiple imputation estimator
+library(tictoc) ## to calculate runtime
 
 # Random seed to be used for each simulation setting
 sim_seed = 11422
 
 # Number of replicates per simulation setting
-num_reps = 100
+num_reps = 1000
 ## Note: You may want to run this code was run in parallel a cluster instead of locally, as it can be slow.
 
 # Set parameters that won't be varied in the loop
 ## These values will be set as the defaults in the sim_data() function for convenience
-fix_N = 340 ## number of neighborhoods (sample size)
+fix_N = 390 ## sample size (Piedmont Triad)
 fix_muU = -0.7 ## error mean
-fix_sigmaU = 0.6 ## error standard deviation
-fix_pV = 0.1 ## proportion of neighborhoods to be queried
+fix_sigmaU = 0.8 ## error standard deviation
+fix_q = 0.1 ## proportion of neighborhoods to be queried
 
 # --------------------------------------------------------------------
 # Function to simulate data (arguments defined as follows)
 ## N = number of neighborhoods (sample size)
-## avg_prev = approximate outcome prevalence
+## beta0 = model intercept
 ## beta1 = log prevalence ratio 
 ## muU = mean of the measurement error distribution
 ## sigmaU = standard deviation of the measurement error distribution
-## pV = proportion of neighborhoods to be queried 
 # --------------------------------------------------------------------
-sim_data = function(N = fix_N, avg_prev, beta1, muU = fix_muU, sigmaU = fix_sigmaU) {
-  ## Get model intercept
-  beta0 = log(avg_prev)
-  
+sim_data = function(N = fix_N, beta0, beta1, muU = fix_muU, sigmaU = fix_sigmaU) {
   ## Simulate true (map-based) proximity to grocery store
   X = rgamma(n = N,
              shape = 1,
@@ -64,27 +63,27 @@ sim_data = function(N = fix_N, avg_prev, beta1, muU = fix_muU, sigmaU = fix_sigm
   return(dat)
 }
 
-# Loop over different average prevalences: 7%, 11%, and 35%
-for (alpha in c(0.07, 0.11, 0.35)) {
-  # And prevalence ratios: log(0.95),  log(0.99), log(1), log(1.01), and log(1.05)
-  for (pr in c(0.95, 0.99, 1, 1.01, 1.05)){
+# Loop over different intercepts: -2.7, -2.2, -1.1
+for (beta0 in c(-2.7, -2.2, -1.1)) {
+  # And log prevalence ratios: -0.05, -0.01, 0, 0.01, 0.05
+  tic(paste("Sims with beta0 =", beta0)) ## Start counting runtime for sims with current intercept beta0
+  for (beta1 in c(-0.05, -0.01, 0, 0.01, 0.05)){
     # Be reproducible
     set.seed(sim_seed) ## set random seed
     
     # Create dataframe to save results for setting
     sett_res = data.frame(sim = paste(sim_seed, 1:num_reps, sep = "-"), 
-                          N = fix_N, beta0 = log(alpha), beta1 = log(pr), muU = fix_muU, sigmaU = fix_sigmaU, pV = fix_pV, avg_prev = NA, ## simulation setting
-                          beta_gs = NA, se_beta_gs = NA, ## gold standard analysis
-                          beta_n = NA, se_beta_n = NA, ## naive analysis
-                          beta_cc = NA, se_beta_cc = NA, ## complete case analysis
-                          beta_imp = NA, se_beta_imp = NA ## imputation analysis
+                          N = fix_N, beta0 = beta0, beta1 = beta1, muU = fix_muU, sigmaU = fix_sigmaU, q = fix_q, avg_prev = NA, ## simulation setting
+                          beta0_gs = NA, se_beta0_gs = NA, beta1_gs = NA, se_beta1_gs = NA, ## gold standard analysis
+                          beta0_n = NA, se_beta0_n = NA, beta1_n = NA, se_beta1_n = NA, ## naive analysis
+                          beta0_cc = NA, se_beta0_cc = NA, beta1_cc = NA, se_beta1_cc = NA, ## complete case analysis
+                          beta0_imp = NA, se_beta0_imp = NA, beta1_imp = NA, se_beta1_imp = NA ## imputation analysis
     )
 
-    # Loop over replicates 
     for (r in 1:num_reps) {
       # Generate data
-      dat = sim_data(avg_prev = alpha, ## average prevalence
-                     beta1 = log(pr)) ## log prevalence ratio
+      dat = sim_data(beta0 = beta0, ## intercept
+                     beta1 = beta1) ## log prevalence ratio
       
       # Save average neighborhood prevalence
       sett_res$avg_prev[r] = mean(dat$Cases / dat$P)
@@ -94,20 +93,20 @@ for (alpha in c(0.07, 0.11, 0.35)) {
                    family = poisson,
                    offset = log(P),
                    data = dat)
-      sett_res$beta_gs[r] = coefficients(fit_gs)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_gs[r] = sqrt(diag(vcov(fit_gs)))[2] ## and its standard error
+      sett_res[r, c("beta0_gs", "beta1_gs")] = coefficients(fit_gs) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_gs", "se_beta1_gs")] = sqrt(diag(vcov(fit_gs))) ## and its standard error
       
       # Fit the gold standard model
       fit_n = glm(formula = Cases ~ Xstar, 
                   family = poisson,
                   offset = log(P),
                   data = dat)
-      sett_res$beta_n[r] = coefficients(fit_n)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_n[r] = sqrt(diag(vcov(fit_n)))[2] ## and its standard error
+      sett_res[r, c("beta0_n", "beta1_n")] = coefficients(fit_n) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_n", "se_beta1_n")] = sqrt(diag(vcov(fit_n))) ## and its standard error
       
       # Select subset of neighborhoods/rows for map-based measures
       query_rows = sample(x = 1:fix_N, 
-                          size = ceiling(fix_pV * fix_N), 
+                          size = ceiling(fix_q * fix_N), 
                           replace = FALSE)
       
       # Make X NA/missing for rows not in selected subset (query_rows)
@@ -118,21 +117,27 @@ for (alpha in c(0.07, 0.11, 0.35)) {
                    family = poisson,
                    offset = log(P),
                    data = dat)
-      sett_res$beta_cc[r] = coefficients(fit_cc)[2] ## estimated log prevalence ratio
-      sett_res$se_beta_cc[r] = sqrt(diag(vcov(fit_cc)))[2] ## and its standard error
-
+      sett_res[r, c("beta0_cc", "beta1_cc")] = coefficients(fit_cc) ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_cc", "se_beta1_cc")] = sqrt(diag(vcov(fit_cc))) ## and its standard error
+      
       # Fit the pooled MI model
-      fit_imp = possum::impPossum(imputation_formula = X ~ Xstar + log(Cases), 
-                                  analysis_formula = Cases ~ X + offset(log(P)), 
-                                  data = dat, 
-                                  B = 20)
-      sett_res$beta_imp[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
-      sett_res$se_beta_imp[r] = fit_imp$Standard.Error[2] ## and its standard error
+      fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Cases), 
+                          analysis_formula = Cases ~ X + offset(log(P)), 
+                          data = dat, 
+                          B = 20)
+      sett_res[r, c("beta0_imp", "beta1_imp")] = fit_imp$Estimate ## estimated log prevalence ratio
+      sett_res[r, c("se_beta0_imp", "se_beta1_imp")] = fit_imp$Standard.Error ## and its standard error
       
       # Save results
       write.csv(x = sett_res,
-                file = paste0("vary_prev/proximity_prev", alpha * 100, "_pr", 100 * pr, "_seed", sim_seed, ".csv"), 
+                file = paste0("vary_prev/proximity_prev", round(exp(beta0) * 100), "_pr", round(exp(beta1) * 100), "_seed", sim_seed, ".csv"), 
                 row.names = F)
     }
   }
+  toc() ## End runtime for sims with current intercept beta0
 }
+
+# Timing from tictoc:
+## Sims with beta0 = -2.7: 216.228 sec elapsed
+## Sims with beta0 = -2.2: 215.851 sec elapsed
+## Sims with beta0 = -1.1: 216.213 sec elapsed
