170224fe9d3b508938ac43a7ff45fa3e40aea7b9
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-04-29 17:49:50 -0400

    Delete unused map data from Fig S6

--- figure-scripts/figS6_histogram_errors_proximity.R
+++ figure-scripts/figS6_histogram_errors_proximity.R
@@ -10,50 +10,53 @@ library(tidycensus) ## to get underlying census tract map shapes
 source("set_api_keys.R")
 
 ############################################################################################
-## LOAD MAP DATA FOR FORSYTH AND BORDERING COUNTIES' CENSUS TRACTS /////////////////////////
-## To align with CDC Places dataset, use 2015 census geometry. /////////////////////////////
+## LOAD FOOD ACCESS DATA FOR PIEDMONT TRIAD ////////////////////////////////////////////////
 ############################################################################################
-# Census tract outlines
-piedmont_triad = c("SURRY", "STOKES", "ROCKINGHAM", "CASWELL", 
-                   "YADKIN", "FORSYTH", "GUILFORD", "ALAMANCE", 
-                   "DAVIE", "DAVIDSON", "RANDOLPH", "MONTGOMERY")
-piedmont_triad_ct = get_acs(state = "NC",
-                            geography = "tract",
-                            county = piedmont_triad,
-                            variables = "B01003_001", ## total population,
-                            geometry = TRUE,
-                            year = 2015) |>
-  dplyr::mutate(GEOID = as.numeric(GEOID)) ### character --> numeric to merge into food access data
-nrow(piedmont_triad_ct) ## N = 388 neighborhoods
-
 ## Proximity to health foods based on straight-line and map-based distances (census tracts)
 food_access = read.csv("https://raw.githubusercontent.com/sarahlotspeich/food/main/piedmont-triad-data/analysis_data.csv")
 nrow(food_access) ## N = 387 neighborhoods (exclude the tract with population = 0)
 
 ## Merge food_access with imputed data and geometry
 map_data = food_access |> 
-  dplyr::mutate(error = X_full - Xstar) |> 
-  dplyr::select(LocationID, 
-                Xstar, 
-                X_full, 
-                error) |> 
-  dplyr::right_join(piedmont_triad_ct, 
-                   by = dplyr::join_by(LocationID == GEOID)) |> 
-  dplyr::select(-variable, -estimate, -moe)
+  dplyr::mutate(add_error = X_full - Xstar, 
+                mult_error = Xstar / X_full) 
 
 ############################################################################################
-## MAKE MAP OF OBSERVED/PREDICTED PROXIMITY ////////////////////////////////////////////////
+## MAKE HISTOGRAM OF ADDITIVE ERROR MAGNITUDES /////////////////////////////////////////////
 ############################################################################################
-map_data |> 
-  ggplot(aes(x = error)) + 
+plot_add_err = map_data |> 
+  ggplot(aes(x = add_error)) + 
   geom_histogram(fill = '#709AE1') + 
   theme_minimal(base_size = 10) + 
-  theme(plot.margin = margin(l=25, r=20, t=20, b=25),
-        legend.position = "bottom", 
-        strip.text = element_text(face = "bold", hjust = 0.5)) + 
-  labs(x = "Magnitude of Additive Error in Straight-Line Proximity", 
+  labs(x = "Magnitude of Additive Error\nin Straight-Line Proximity", 
        y = "Number of Census Tracts")
+ggsave(plot = plot_add_err, 
+       filename = "~/Documents/food/figures/figS6a_histogram_add_errors_piedmont.png", 
+       device = "png", 
+       width = 7, 
+       height = 5, 
+       units = "in")
 
+############################################################################################
+## MAKE HISTOGRAM OF MULTIPLICATIVE ERROR MAGNITUDES ///////////////////////////////////////
+############################################################################################
+plot_mult_err = map_data |> 
+  ggplot(aes(x = mult_error)) + 
+  geom_histogram(fill = "#BACBEE") + 
+  theme_minimal(base_size = 10) + 
+  labs(x = "Magnitude of Multiplicative Error\nin Straight-Line Proximity", 
+       y = "Number of Census Tracts")
+ggsave(plot = plot_mult_err, 
+       filename = "~/Documents/food/figures/figS6b_histogram_mult_errors_piedmont.png", 
+       device = "png", 
+       width = 7, 
+       height = 5, 
+       units = "in")
+
+############################################################################################
+## COMBINE PLOTS ///////////////////////////////////////////////////////////////////////////
+############################################################################################
+ggpubr::ggarrange(plot_add_err, plot_mult_err, nrow = 1)
 ggsave(filename = "~/Documents/food/figures/figS6_histogram_errors_piedmont.png", 
        device = "png", 
        width = 7, 
