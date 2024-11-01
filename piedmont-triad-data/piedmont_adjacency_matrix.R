fadeb9a40689e38e91d25201d77f96470348c635
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-11-01 07:53:15 -0400

    Move API key

--- piedmont-triad-data/piedmont_adjacency_matrix.R
+++ piedmont-triad-data/piedmont_adjacency_matrix.R
@@ -5,16 +5,16 @@
 library(tidycensus) ## for shapefiles
 library(spdep) ## for adjacency matrix
 
+# Set API keys (redacted to avoid violating use agreements)
+## See ex_set_api_keys.R to set up your own script
+# source("set_api_keys.R")
+
 ############################################################################################
 ## LOAD FOOD ACCESS DATA FOR FORSYTH AND BORDERING COUNTIES' CENSUS TRACTS /////////////////
 ############################################################################################
 ## Proximity to health foods based on straight-line and map-based distances (census tracts)
 food_access = read.csv("https://raw.githubusercontent.com/sarahlotspeich/food_access_imputation/main/piedmont-triad-data/analysis_data.csv")
 nrow(food_access) ## N = 387 neighborhoods (exclude the tract with population = 0)
-
-# Set API keys (redacted to avoid violating use agreements)
-## See ex_set_api_keys.R to set up your own script
-# source("set_api_keys.R")
 
 # Define Piedmont Triad counties
 piedmont_triad = c("SURRY", "STOKES", "ROCKINGHAM", "CASWELL", 
