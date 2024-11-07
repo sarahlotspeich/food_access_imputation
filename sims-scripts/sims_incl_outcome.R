aa7b958d07f3eaaaa07712f840fdd9bb3eb2fd60
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-11-07 16:21:05 -0500

    Commit sims for Fig S3 with new params (.R, .csv)

--- sims-scripts/sims_incl_outcome.R
+++ sims-scripts/sims_incl_outcome.R
@@ -18,21 +18,20 @@ num_reps = 1000
 
 # Set parameters that won't be varied in the loop
 ## These values will be set as the defaults in the sim_data() function for convenience
-fix_beta0 = -2.7 ## outcome model intercept (leads to ~ 7% prevalence)
-fix_beta1 = log(1.01) ## log prevalence ratio
-fix_muU = -0.7 ## error mean
-fix_sigmaU = 0.8 ## error standard deviation
-fix_q = 0.1 ## proportion to be queried
+### Some were based on the Piedmont Triad data 
+beta0 = -2.2 ## outcome model intercept (leads to ~ 11% prevalence, based on diabetes)
+muU = -0.7 ## error mean
+sigmaU = 0.8 ## error standard deviation
+lambdaPOP = 4095 ## average population per census tract
+### And others were experimental 
+beta1 = log(1.01) ## log prevalence ratio for X on Y
+q = 0.1 ## proportion of neighborhoods to be queried
 
 # --------------------------------------------------------------------
 # Function to simulate data (arguments defined as follows)
 ## N = number of neighborhoods (sample size)
-## beta0 = model intercept
-## beta1 = log prevalence ratio 
-## muU = mean of the measurement error distribution
-## sigmaU = standard deviation of the measurement error distribution
 # --------------------------------------------------------------------
-sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, muU = fix_muU, sigmaU = fix_sigmaU) {
+sim_data = function(N) {
   ## Simulate true (map-based) proximity to grocery store
   X = rgamma(n = N,
              shape = 1,
@@ -49,23 +48,23 @@ sim_data = function(N, beta0 = fix_beta0, beta1 = fix_beta1, muU = fix_muU, sigm
   Xstar = X + U ### assuming additive measurement error model
   
   ## Simulate population
-  P = rpois(n = N, 
-            lambda = 4165)
+  POP = rpois(n = N, 
+              lambda = lambdaPOP)
   
-  ## Simulate cases of health outcome
+  ## Simulate Y of health outcome
   lambda = exp(beta0 + beta1 * X)
-  Cases = rpois(n = N, 
-                lambda = P * lambda)
+  Y = rpois(n = N, 
+            lambda = POP * lambda)
   
   ## Create dataset
-  dat = data.frame(id = 1:N, X, Xstar, P, Cases)
+  dat = data.frame(id = 1:N, X, Xstar, POP, Y)
   
   # Return dataset
   return(dat)
 }
 
-# Loop over different sample sizes: N = 390 (Piedmont Triad), 2200 (all of NC)
-for (N in c(390, 2200)) {
+# Loop over different sample sizes: N = 387 (Piedmont Triad), 2169 (all of NC)
+for (N in c(387, 2169)) {
   tic(paste("Sims with N =", N)) ## Start counting runtime for sims with current sample size N
   
   # Be reproducible
@@ -73,7 +72,7 @@ for (N in c(390, 2200)) {
   
   # Create dataframe to save results for setting
   sett_res = data.frame(sim = paste(sim_seed, 1:num_reps, sep = "-"), 
-                        N, beta1 = fix_beta1, muU = fix_muU, sigmaU = fix_sigmaU, q = fix_q, avg_prev = NA, ## simulation setting
+                        N, beta1 = beta1, muU = muU, sigmaU = sigmaU, q = q, avg_prev = NA, ## simulation setting
                         beta_noY = NA, se_beta_noY = NA, ## impute with E(X|X*)
                         beta_Y = NA, se_beta_Y = NA, ## impute with E(X|X*, Y)
                         beta_logY = NA, se_beta_logY = NA, ## impute with E(X|X*, log(Y))
@@ -87,11 +86,11 @@ for (N in c(390, 2200)) {
     dat = sim_data(N  = N) ## sample size
     
     # Save average neighborhood prevalence
-    sett_res$avg_prev[r] = mean(dat$Cases / dat$P)
+    sett_res$avg_prev[r] = mean(dat$Y / dat$POP)
 
     # Select subset of neighborhoods/rows for map-based measures
     query_rows = sample(x = 1:N, 
-                        size = ceiling(fix_q * N), 
+                        size = ceiling(q * N), 
                         replace = FALSE)
     
     # Make X NA/missing for rows not in selected subset (query_rows)
@@ -99,39 +98,39 @@ for (N in c(390, 2200)) {
     
     # 1. Fit the pooled MI model with E(X|X*)
     fit_imp = impPossum(imputation_formula = X ~ Xstar, 
-                        analysis_formula = Cases ~ X + offset(log(P)), 
+                        analysis_formula = Y ~ X + offset(log(POP)), 
                         data = dat, 
                         B = 20)
     sett_res$beta_noY[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
     sett_res$se_beta_noY[r] = fit_imp$Standard.Error[2] ## and its standard error
     
     # 2. Fit the pooled MI model with E(X|X*, Y)
-    fit_imp = impPossum(imputation_formula = X ~ Xstar + Cases, 
-                        analysis_formula = Cases ~ X + offset(log(P)), 
+    fit_imp = impPossum(imputation_formula = X ~ Xstar + Y, 
+                        analysis_formula = Y ~ X + offset(log(POP)), 
                         data = dat, 
                         B = 20)
     sett_res$beta_Y[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
     sett_res$se_beta_Y[r] = fit_imp$Standard.Error[2] ## and its standard error
     
     # 3. Fit the pooled MI model with E(X|X*, log(Y))
-    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Cases), 
-                        analysis_formula = Cases ~ X + offset(log(P)), 
+    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Y), 
+                        analysis_formula = Y ~ X + offset(log(POP)), 
                         data = dat, 
                         B = 20)
     sett_res$beta_logY[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
     sett_res$se_beta_logY[r] = fit_imp$Standard.Error[2] ## and its standard error
     
     # 4. Fit the pooled MI model with E(X|X*, log(Y/Pop))
-    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Cases/P), 
-                        analysis_formula = Cases ~ X + offset(log(P)), 
+    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Y/POP), 
+                        analysis_formula = Y ~ X + offset(log(POP)), 
                         data = dat, 
                         B = 20)
     sett_res$beta_logYoverPop[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
     sett_res$se_beta_logYoverPop[r] = fit_imp$Standard.Error[2] ## and its standard error
     
     # 5. Fit the pooled MI model with E(X|X*, log(Y), log(Pop))
-    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Cases) + log(P), 
-                        analysis_formula = Cases ~ X + offset(log(P)), 
+    fit_imp = impPossum(imputation_formula = X ~ Xstar + log(Y) + log(POP), 
+                        analysis_formula = Y ~ X + offset(log(POP)), 
                         data = dat, 
                         B = 20)
     sett_res$beta_logY_logPop[r] = fit_imp$Estimate[2] ## estimated log prevalence ratio
@@ -139,12 +138,12 @@ for (N in c(390, 2200)) {
     
     # Save results
     write.csv(x = sett_res,
-              file = paste0("sims-data/include_outcome/proximity_N", N, "_q", 100 * fix_q, "_seed", sim_seed, ".csv"), 
+              file = paste0("include_outcome/proximity_N", N, "_q", 100 * q, "_seed", sim_seed, ".csv"), 
               row.names = F)
   }
   toc() ## End runtime for sims with current sample size N
 }
 
 # Timing from tictoc:
-## Sims with N = 390: 150.197 sec elapsed
+## Sims with N = 387: 156.067 sec elapsed
 ## Sims with N = 2200: 440.436 sec elapsed
