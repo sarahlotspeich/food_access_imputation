724c62e4362dd37a151dcfede8317393bdf1d6ab
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-01-05 15:31:08 -0800

    Remake Fig 3 with METRO (.R, .png, .pdf)

--- figure-scripts/fig3_scatterplot_proximity_piedmont.R
+++ figure-scripts/fig3_scatterplot_proximity_piedmont.R
@@ -15,8 +15,8 @@ nrow(food_access) ## N = 387 neighborhoods (exclude the tract with population = 
 ## MAKE LONG VERSION OF THE DATA TO PLOT ///////////////////////////////////////////////////
 ############################################################################################
 plot_dat = food_access |> 
-  dplyr::select(LocationID, CountyName, Xstar, X_full, X_partial) |> 
-  tidyr::gather(key = "Dataset", value = "X", -c(1:3)) |> 
+  dplyr::select(GEOID, CountyName, METRO, Xstar, X_full, X_partial) |> 
+  tidyr::gather(key = "Dataset", value = "X", -c(1:4)) |> 
   dplyr::mutate(Dataset = factor(x = Dataset, 
                                  levels = c("X_full", "X_partial"), 
                                  labels = c("Fully Queried", "Partially Queried")))
@@ -25,23 +25,31 @@ plot_dat = food_access |>
 ## MAKE SCATTER PLOT OF STRAIGHT-LINE VS MAP-BASED PROXIMITY ///////////////////////////////
 ############################################################################################
 plot_dat |> 
+  dplyr::mutate(METRO = factor(x = METRO, 
+                               levels = c(TRUE, FALSE))) |> 
   ggplot(aes(x = Xstar, 
-             y = X)) + 
-  geom_point() + 
+             y = X, 
+             color = METRO)) + 
+  geom_point(alpha = 0.5) + 
   geom_smooth(method = "lm", 
               se = FALSE, 
-              col = '#FD7446', 
+              #col = '#FD7446', 
               size = 1, 
-              fullrange = TRUE) + 
+              fullrange = TRUE, 
+              alpha = 0.5) + 
   geom_abline(slope = 1, 
               intercept = 0, 
-              col = '#709AE1', 
+              col = "grey", # col = '#709AE1', 
               size = 1, 
               linetype = 2) + 
   theme_minimal(base_size = 10) + 
   theme(plot.margin = margin(l=25, r=20, t=20, b=25), 
         strip.background = element_rect(fill = "black"),
-        strip.text = element_text(color = "white")) + 
+        strip.text = element_text(color = "white"), 
+        legend.box = "vertical", 
+        legend.position = "top") + 
+  scale_color_manual(name = "Metropolitan Area:", 
+                     values = c("#6F98DF", "#EB7B51")) + 
   labs(x = "Straight-Line Proximity to Healthy Foods (X*)",
        y = "Map-Based Proximity to Healthy Foods (X)") + 
   xlim(c(0, 16)) +
@@ -49,12 +57,12 @@ plot_dat |>
   coord_equal() + 
   facet_wrap(~Dataset)
 
-ggsave(filename = "figures/fig3_scatterplot_proximity_piedmont.png", 
+ggsave(filename = "figures/rev_fig3_scatterplot_proximity_piedmont.png", 
        device = "png", 
        width = 10, 
        height = 5, 
        units = "in")
-ggsave(filename = "figures/fig3_scatterplot_proximity_piedmont.pdf", 
+ggsave(filename = "figures/rev_fig3_scatterplot_proximity_piedmont.pdf", 
        device = "pdf", 
        width = 10, 
        height = 5, 
