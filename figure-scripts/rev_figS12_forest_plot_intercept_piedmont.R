53c8e7d2ade0119a77e5fc08032d6cb0eb72b84d
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-01-10 10:04:23 -0500

    Revised Fig S12 (.R, .png, .pdf)

--- figure-scripts/rev_figS12_forest_plot_intercept_piedmont.R
+++ figure-scripts/rev_figS12_forest_plot_intercept_piedmont.R
@@ -27,29 +27,55 @@ plot_data |>
                                              "Complete\nCase",
                                              "Imputation",
                                              "Gold\nStandard")),
+                Outcome = factor(x = Outcome, 
+                                 levels = c("Diagnosed Diabetes", "Obesity"), 
+                                 labels = c("Outcome: Diagnosed Diabetes", "Outcome: Obesity")),
                 Spatial = ifelse(test = Spatial, 
                                  yes = "Spatial", 
-                                 no = "Non-Spatial")) |>
-  ggplot(aes(x = Analysis, y = Est, col = Spatial)) +
-  geom_point(size = 3, position = position_dodge(width = 0.8)) +
-  geom_errorbar(lwd = 1.5, aes(ymin = LB, ymax = UB), position = position_dodge(width = 0.8)) +
-  scale_color_manual(name = "Model:", 
-                     values = c("#EB7B51", "#6F98DF")) + 
+                                 no = "Non-Spatial"),
+                Spatial = factor(x = Spatial, 
+                                 levels = c("Non-Spatial", "Spatial")),
+                Coefficient = factor(x = Coefficient, 
+                                     levels = c("(Intercept)", "X_full", "X_partial", 
+                                                "Xstar", "METROTRUE", "METROTRUE:X_full", 
+                                                "METROTRUE:X_partial", "METROTRUE:Xstar"), 
+                                     labels = c("Coefficient: Baseline Prevalence", "Coefficient: Proximity", "Coefficient: Proximity", 
+                                                "Coefficient: Proximity", "Coefficient: Metropolitan", "Coefficient: Proximity x Metropolitan", 
+                                                "Coefficient: Proximity x Metropolitan", "Coefficient: Proximity x Metropolitan")), 
+                Null = ifelse(test = LB < 1 & UB > 1, 
+                              yes = "Contains 1", 
+                              no = "Does Not Contain 1")) |> 
+  ggplot(aes(x = Analysis, y = Est, 
+             shape = Spatial, group = Spatial, linetype = Spatial)) + 
+  geom_errorbar(aes(ymin = LB, ymax = UB), 
+                lwd = 1.5, 
+                position = position_dodge(width = 1), 
+                alpha = 1, 
+                color = "#6F98DF") + 
+  geom_point(size = 3, 
+             position = position_dodge(width = 1), 
+             color = "#6F98DF") + 
+  facet_wrap(. ~ Outcome + Coefficient, 
+             scales = "free") + 
   theme_minimal(base_size = 12) +
   theme(legend.position = "top", 
         strip.background = element_rect(fill = "black"),
         strip.text = element_text(color = "white"), 
-        legend.box = "vertical") +
+        legend.box = "horizontal", 
+        legend.direction = "horizontal") +
+  scale_shape_manual(name = "Model", 
+                     values = c(15, 16)) + 
+  scale_linetype_manual(name = "Model", 
+                        values = c(1, 11)) + 
   xlab("Analysis Method") +
-  ylab("Baseline Prevalence") + 
-  facet_wrap(~ Outcome, scales = "free")
+  ylab("Baseline Prevalence (95% Confidence Interval)")
 
-ggsave(filename = "figures/rev_figS11_forest_plot_intercept_piedmont.png", 
+ggsave(filename = "figures/rev_figS12_forest_plot_intercept_piedmont.png", 
        device = "png", 
        width = 10, 
        height = 5, 
        units = "in")
-ggsave(filename = "figures/rev_figS11_forest_plot_intercept_piedmont.pdf", 
+ggsave(filename = "figures/rev_figS12_forest_plot_intercept_piedmont.pdf", 
        device = "pdf", 
        width = 10, 
        height = 5, 
